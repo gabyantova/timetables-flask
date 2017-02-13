a4ca779387029f8e6aff22f737f8accd4ec1d7b8
gabyantova <gaby.antova@gmail.com>
2017-02-13 11:08:48 +0000

    Ico did this

--- content_management.py
+++ content_management.py
@@ -35,9 +35,23 @@ def Get_course_dict():
         cols = row.find_all("td")
         col_list = []
         url = cols[0].find("a").get("href")
+        drps = cols[1].find("a").get("href")
         for col in cols:
           col_list.append(col.text)
-        COURSE_DICT[(col_list[2]).strip()] = {"course":col_list[0], "year":col_list[9], "credits":col_list[8], "course_url":url}
+        course_type = []
+        for x in range(3,7):
+            if col_list[x]:
+                course_type.append(col_list[x])
+
+        COURSE_DICT[(col_list[2]).strip()] = {"course":col_list[0],
+                                              "year":col_list[9],
+                                              "credits":col_list[8],
+                                              "course_url":url,
+                                              "types":course_type,
+                                              "drps":drps,
+                                              "level":col_list[7]}
+
+    # print COURSE_DICT
 
     return COURSE_DICT
 
@@ -70,8 +84,10 @@ def Get_UOE_venue_data():
     #var_list[0] = var_list[0].split("=")[1]
     var_list[1] = var_list[1].split("=")[1].strip()
     #mapAreas = ast.literal_eval(var_list[0])
+
     pointsOfInterest = ast.literal_eval(var_list[1])
 
+    # print pointsOfInterest
     return pointsOfInterest
 
 
@@ -107,12 +123,16 @@ def Get_content_dict():
                             dict_course_that_hour = {}
                             first_split = course_that_hour.text.split("[", 1) #split on first square bracket
                             second_split = first_split[1].split("]", 1)
+                            room = second_split[1].split(",")[0].strip()
                             array_course_that_hour.append(first_split[0])
                             array_course_that_hour.extend(second_split)
-                            dict_course_that_hour = {'course_acr' : array_course_that_hour[0].strip(), 'course_year' : array_course_that_hour[1].strip(), 'course_details' : array_course_that_hour[2]}
+                            dict_course_that_hour = {'course_acr' : array_course_that_hour[0].strip(),
+                                                     'course_year' : array_course_that_hour[1].strip(),
+                                                     'course_details' : array_course_that_hour[2],
+                                                     'course_room': room}
                             final_array_courses_that_hour.append(dict_course_that_hour)
                     semester_dict[day_of_the_week].append(final_array_courses_that_hour)
 
         semesters_list.append(semester_dict)
-    print semesters_list[1]
+    #print semesters_list[1]
     return semesters_list
