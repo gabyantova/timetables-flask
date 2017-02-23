be88024aa4509137710685c2d0c2889c3d200fac
gabyantova <gaby.antova@gmail.com>
2017-02-23 12:33:56 +0000

    everything's broken

--- content_management.py
+++ content_management.py
@@ -5,10 +5,8 @@ import re
 import ast
 
 
-
-
 def myFunc():
-    return("HELLO BITCHES!!!!")
+    return ("HELLO BITCHES!!!!")
 
 
 def Weekdays():
@@ -18,11 +16,11 @@ def Weekdays():
 def Get_url():
     now = datetime.datetime.now()
     if now.month < 8:
-        ac_year_start = now.year%100 - 1
+        ac_year_start = now.year % 100 - 1
     else:
-        ac_year_start = now.year%100
-    return "http://web.inf.ed.ac.uk/infweb/student-services/ito/admin/timetables/lecture-timetable-20" + str(ac_year_start) + "-" + str(ac_year_start + 1)
-
+        ac_year_start = now.year % 100
+    return "http://web.inf.ed.ac.uk/infweb/student-services/ito/admin/timetables/lecture-timetable-20" + str(
+        ac_year_start) + "-" + str(ac_year_start + 1)
 
 
 def Get_course_dict():
@@ -37,53 +35,60 @@ def Get_course_dict():
         url = cols[0].find("a").get("href")
         drps = cols[1].find("a").get("href")
         for col in cols:
-          col_list.append(col.text)
+            col_list.append(col.text)
         course_type = []
-        for x in range(3,7):
+        for x in range(3, 7):
             if col_list[x]:
                 course_type.append(col_list[x])
 
-        COURSE_DICT[(col_list[2]).strip()] = {"course":col_list[0],
-                                              "year":col_list[9],
-                                              "credits":col_list[8],
-                                              "course_url":url,
-                                              "types":course_type,
-                                              "drps":drps,
-                                              "level":col_list[7]}
+        COURSE_DICT[(col_list[2]).strip()] = {"course": col_list[0],
+                                              "year": col_list[9],
+                                              "credits": col_list[8],
+                                              "course_url": url,
+                                              "types": course_type,
+                                              "drps": drps,
+                                              "level": col_list[7]}
 
-    # print COURSE_DICT
+    def pretty(d, indent=0):
+        for key, value in d.iteritems():
+            print '\t' * indent + str(key)
+            if isinstance(value, dict):
+                pretty(value, indent + 1)
+            else:
+                print '\t' * (indent + 1) + str(value)
+
+    print pretty(COURSE_DICT)
 
     return COURSE_DICT
-
 
 
 def Get_venue_dict():
     VENUE_DICT = {}
     url_venue = "http://web.inf.ed.ac.uk/infweb/student-services/ito/admin/venue-codes"
     soup_venue = BeautifulSoup(urllib.urlopen(url_venue).read())
-    #course_table = soup_venue.table.tbody.find_all("tr")
+    # course_table = soup_venue.table.tbody.find_all("tr")
 
-    building_and_room_codes = soup_venue.find("section", {"id":"block-system-main"}).find_all("ul")
+    building_and_room_codes = soup_venue.find("section", {"id": "block-system-main"}).find_all("ul")
 
     building_codes = building_and_room_codes[0].find_all("li")
-    #room_codes = building_and_room_codes[1].find_all("li")
+    # room_codes = building_and_room_codes[1].find_all("li")
 
 
     for building_code in building_codes:
-        VENUE_DICT[building_code.find("strong").get_text().replace(" ", "")] = {"venue_name": building_code.find("a").get_text(),
-                                                               "venue_url": building_code.find("a").get("href")}
+        VENUE_DICT[building_code.find("strong").get_text().replace(" ", "")] = {
+            "venue_name": building_code.find("a").get_text(),
+            "venue_url": building_code.find("a").get("href")}
 
     return VENUE_DICT
-
 
 
 def Get_UOE_venue_data():
     soup = BeautifulSoup(urllib.urlopen("http://webproxy.is.ed.ac.uk/web-proxy/maps_edweb/data.php"))
     content = soup.body.text
     var_list = content.split(";")
-    #var_list[0] = var_list[0].split("=")[1]
+    # var_list[0] = var_list[0].split("=")[1]
     var_list[1] = var_list[1].split("=")[1].strip()
-    #mapAreas = ast.literal_eval(var_list[0])
+    # mapAreas = ast.literal_eval(var_list[0])
 
     pointsOfInterest = ast.literal_eval(var_list[1])
 
@@ -114,25 +119,25 @@ def Get_content_dict():
                     courses_that_hour_p = row_item.find_all("p")
                     courses_that_hour_div = row_item.find_all("div")
                     courses_that_hour = courses_that_hour_p + courses_that_hour_div
-                    if not(courses_that_hour):
-                        courses_that_hour = [row_item]#.find_all("td")
+                    if not (courses_that_hour):
+                        courses_that_hour = [row_item]  # .find_all("td")
                     final_array_courses_that_hour = []
                     for course_that_hour in courses_that_hour:
                         if re.search("\[[0-9]\]", course_that_hour.text):
                             array_course_that_hour = []
                             dict_course_that_hour = {}
-                            first_split = course_that_hour.text.split("[", 1) #split on first square bracket
+                            first_split = course_that_hour.text.split("[", 1)  # split on first square bracket
                             second_split = first_split[1].split("]", 1)
                             room = second_split[1].split(",")[0].strip()
                             array_course_that_hour.append(first_split[0])
                             array_course_that_hour.extend(second_split)
-                            dict_course_that_hour = {'course_acr' : array_course_that_hour[0].strip(),
-                                                     'course_year' : array_course_that_hour[1].strip(),
-                                                     'course_details' : array_course_that_hour[2],
+                            dict_course_that_hour = {'course_acr': array_course_that_hour[0].strip(),
+                                                     'course_year': array_course_that_hour[1].strip(),
+                                                     'course_details': array_course_that_hour[2],
                                                      'course_room': room}
                             final_array_courses_that_hour.append(dict_course_that_hour)
                     semester_dict[day_of_the_week].append(final_array_courses_that_hour)
 
         semesters_list.append(semester_dict)
-    #print semesters_list[1]
+    # print semesters_list[1]
     return semesters_list
