a9efa37965a4c29abce0b3ba6c5c5e3686378a6e
gabyantova <gaby.antova@gmail.com>
2017-03-09 22:16:50 +0000

    filtering with internet code works as intended

--- static/script.js
+++ static/script.js
@@ -2,23 +2,38 @@
 var pinnedCourses = [];
 
 
-$(function () {
+$(document).ready(function () {
 
-    var bySubject = [], byYear = [], byLocation = [];
-
+    var bySubject = ["[data-category~='CS']", "[data-category~='AI']", "[data-category~='CG']", "[data-category~='SE']"], byYear = ["[data-category~='year1']", "[data-category~='year2']", "[data-category~='year3']", "[data-category~='year4']", "[data-category~='year5']"], byYear2 = [], byYear3 = [], byYear4 = [], byYear5 = [], byLocation = [];
+    // var bySubject = [], byYear = [], byYear2 = [], byYear3 = [], byYear4 = [], byYear5 = [], byLocation = [];
     $("input[name=subject]").on("change", function () {
         if (this.checked) bySubject.push("[data-category~='" + $(this).attr("value") + "']");
         else removeA(bySubject, "[data-category~='" + $(this).attr("value") + "']");
     });
-
     $("input[name=year]").on("change", function () {
         if (this.checked) byYear.push("[data-category~='" + $(this).attr("value") + "']");
         else removeA(byYear, "[data-category~='" + $(this).attr("value") + "']");
     });
+    /* $("input[name=year2]").on("change", function () {
+     if (this.checked) byYear2.push("[data-category~='" + $(this).attr("value") + "']");
+     else removeA(byYear2, "[data-category~='" + $(this).attr("value") + "']");
+     });
+     $("input[name=year3]").on("change", function () {
+     if (this.checked) byYear3.push("[data-category~='" + $(this).attr("value") + "']");
+     else removeA(byYear3, "[data-category~='" + $(this).attr("value") + "']");
+     });
+     $("input[name=year4]").on("change", function () {
+     if (this.checked) byYear4.push("[data-category~='" + $(this).attr("value") + "']");
+     else removeA(byYear4, "[data-category~='" + $(this).attr("value") + "']");
+     });
+     $("input[name=year5]").on("change", function () {
+     if (this.checked) byYear5.push("[data-category~='" + $(this).attr("value") + "']");
+     else removeA(byYear5, "[data-category~='" + $(this).attr("value") + "']");
+     });*/
 
     $("input").on("change", function () {
         var str = "Include items \n";
-        var selector = '', yearSelector = '';
+        var selector = '', yearSelector = '', yearSelector2 = '', yearSelector3 = '', yearSelector4 = '', yearSelector5 = '';
 
         var $lis = $('.table >tbody >tr >td > .subject'), $checked = $('input:checked');
 
@@ -35,10 +50,22 @@ $(function () {
                         }
                     });
                 } else {
+                    /*str += "    AND " + "with (" + bySubject.join(' OR ') + ")\n";
+                     $($('input[name=subject]:checked')).each(function (index, bySubject) {
+                     selector += "[data-category~='" + bySubject.id + "']";
+                     });*/
+
+
                     str += "    AND " + "with (" + bySubject.join(' OR ') + ")\n";
                     $($('input[name=subject]:checked')).each(function (index, bySubject) {
-                        selector += "[data-category~='" + bySubject.id + "']";
+                        if (selector === '') {
+                            selector += "[data-category~='" + bySubject.id + "']";
+                        } else {
+                            selector += ",[data-category~='" + bySubject.id + "']";
+                        }
                     });
+
+
                 }
             }
 
@@ -63,10 +90,96 @@ $(function () {
                     });
                 }
             }
+            /*
+             if (byYear2.length) {
+             if (str == "Include items \n") {
+             str += "    " + "with (" + byYear2.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear2) {
+             if (selector === '') {
+             selector += "[data-category~='" + byYear2.id + "']";
+             } else {
+             selector += ",[data-category~='" + byYear2.id + "']";
+             }
+             });
+             } else {
+             str += "    AND " + "with (" + byYear.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear2) {
+             if (yearSelector2 === '') {
+             yearSelector2 += "[data-category~='" + byYear2.id + "']";
+             } else {
+             yearSelector2 += ",[data-category~='" + byYear2.id + "']";
+             }
+             });
+             }
+             }
+             if (byYear3.length) {
+             if (str == "Include items \n") {
+             str += "    " + "with (" + byYear3.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear3) {
+             if (selector === '') {
+             selector += "[data-category~='" + byYear3.id + "']";
+             } else {
+             selector += ",[data-category~='" + byYear3.id + "']";
+             }
+             });
+             } else {
+             str += "    AND " + "with (" + byYear.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear3) {
+             if (yearSelector3 === '') {
+             yearSelector3 += "[data-category~='" + byYear3.id + "']";
+             } else {
+             yearSelector3 += ",[data-category~='" + byYear3.id + "']";
+             }
+             });
+             }
+             }
+             if (byYear4.length) {
+             if (str == "Include items \n") {
+             str += "    " + "with (" + byYear4.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear4) {
+             if (selector === '') {
+             selector += "[data-category~='" + byYear4.id + "']";
+             } else {
+             selector += ",[data-category~='" + byYear4.id + "']";
+             }
+             });
+             } else {
+             str += "    AND " + "with (" + byYear.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear4) {
+             if (yearSelector4 === '') {
+             yearSelector4 += "[data-category~='" + byYear4.id + "']";
+             } else {
+             yearSelector4 += ",[data-category~='" + byYear4.id + "']";
+             }
+             });
+             }
+             }
+             if (byYear4.length) {
+             if (str == "Include items \n") {
+             str += "    " + "with (" + byYear5.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear5) {
+             if (selector === '') {
+             selector += "[data-category~='" + byYear5.id + "']";
+             } else {
+             selector += ",[data-category~='" + byYear5.id + "']";
+             }
+             });
+             } else {
+             str += "    AND " + "with (" + byYear.join(' OR ') + ")\n";
+             $($('input[name=year]:checked')).each(function (index, byYear5) {
+             if (yearSelector5 === '') {
+             yearSelector5 += "[data-category~='" + byYear5.id + "']";
+             } else {
+             yearSelector5 += ",[data-category~='" + byYear5.id + "']";
+             }
+             });
+             }
+             }*/
 
             $lis.hide();
             console.log(selector);
             console.log(yearSelector);
+            // if (yearSelector === '' && yearSelector2 === '' && yearSelector3 === '' && yearSelector4 === '' && yearSelector5 === '') {
             if (yearSelector === '') {
                 $lis.filter(selector).show();
             } else {
@@ -94,15 +207,14 @@ $(function () {
     }
 });
 
-$( document ).ready( function(){
+$(document).ready(function () {
     $("#semester1-table").addClass("active");
-    var checkboxes = $( ':checkbox' );
+    var checkboxes = $(':checkbox');
 
     // Check all checkboxes
-    checkboxes.prop( 'checked', true );
+    checkboxes.prop('checked', true);
 
 });
-
 
 
 // $("#showPinnedCoursesButton").click(function() {
